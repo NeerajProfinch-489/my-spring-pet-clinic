886f569017439d5d807f4bd908ba3b3669fd12c8
NeerajProfinch-489 <neeraj.patel@profinch.com>
2020-03-02 18:21:51 +0530

    Created Index view page and indexController . Closed #8 #7

---
+++ src/main/java/neeraj/springframework/myspringpetclinic/controller/OwnerController.java
@@ -0,0 +1,12 @@
+package neeraj.springframework.myspringpetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping("/owner")
+    public String ownerIndex(){
+        return "owners/index";
+    }
+}
