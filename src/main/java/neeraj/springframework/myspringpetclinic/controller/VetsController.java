945feefa8b5d6520ad919dc4ad86f8118d21110b
NeerajProfinch-489 <neeraj.patel@profinch.com>
2020-03-04 12:30:08 +0530

    Listed all the owner and vets in index.html . Closed #14

--- src/main/java/neeraj/springframework/myspringpetclinic/controller/VetsController.java
+++ src/main/java/neeraj/springframework/myspringpetclinic/controller/VetsController.java
@@ -1,12 +1,21 @@
 package neeraj.springframework.myspringpetclinic.controller;
 
+import neeraj.springframework.myspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String vetIndex(){
+    public String vetIndex(Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index.html";
     }
 }
