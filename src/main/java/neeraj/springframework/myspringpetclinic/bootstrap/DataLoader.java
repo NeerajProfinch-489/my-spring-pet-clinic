0e17e81736a90416386097ef41eb2c107a2851a1
NeerajProfinch-489 <neeraj.patel@profinch.com>
2020-03-04 17:07:01 +0530

    Updated map impl to manage setting of ID property. closed #22

--- src/main/java/neeraj/springframework/myspringpetclinic/bootstrap/DataLoader.java
+++ src/main/java/neeraj/springframework/myspringpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,6 @@ import neeraj.springframework.myspringpetclinic.model.Owner;
 import neeraj.springframework.myspringpetclinic.model.Vet;
 import neeraj.springframework.myspringpetclinic.services.OwnerService;
 import neeraj.springframework.myspringpetclinic.services.VetService;
-import neeraj.springframework.myspringpetclinic.services.map.OwnerServicesMap;
-import neeraj.springframework.myspringpetclinic.services.map.VetServicesMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -22,14 +20,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Neeraj");
         owner1.setLastName("Patel");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Keerthi");
         owner2.setLastName("Keerthi's Last name");
 
@@ -38,14 +34,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Owner loaded....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jassie");
         vet2.setLastName("Porter");
 
